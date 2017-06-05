75e6e26e9b094e6802f464100f04625eaacc9157
mingxinzhang <mnzmx_z@163.com>
2017-06-05 22:33:17 +0800

    修改

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
     $db_conf = array(
         'servername' => 'localhost',
         'username' => 'root',
-        'password' => 'zmx093032#',
+        'password' => 'passgz6374',
         'dbname' => 'shouzhangcom'
     );
 ?>
